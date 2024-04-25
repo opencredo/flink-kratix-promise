2b07728df607c8da3df352db1848aa8878b8dda1
Howard <139238193+howard-oc@users.noreply.github.com>
2024-04-25 10:00:15 +0100

    Fix/issue 38 (#39)

--- internal/configure-pipeline/src/main.rs
+++ internal/configure-pipeline/src/main.rs
@@ -1,31 +1,18 @@
-use std::{env,process};
-use log::{LevelFilter};
-use log4rs::append::console::ConsoleAppender;
-use log4rs::config::{Appender, Root};
-use log4rs::Config;
-
-
+use kratix_utils::pipeline::PipelineConfig;
+use kratix_utils::run_pipeline;
+use kratix_utils::ResourceRequest;
+use std::{env, process};
 
 fn main() {
-    
-
-    let stdout = ConsoleAppender::builder().build();    
-    let config = Config::builder()
-        .appender(Appender::builder().build("stdout", Box::new(stdout)))
-        .build(Root::builder().appender("stdout").build(LevelFilter::Trace))
-        .unwrap(); 
-       
-    let _handle = log4rs::init_config(config).unwrap();
-
     let args: Vec<String> = env::args().collect();
 
-    // TODO: pass function to do IOC transform
     match args[1].as_str() {
-        "pipeline" => kratix_utils::run_pipeline(args),
+        "pipeline" => {
+            let _result = run_pipeline();
+        }
         _ => {
             log::warn!("Unknown command: {}", args[1]);
             process::exit(1);
         }
     }
 }
-
