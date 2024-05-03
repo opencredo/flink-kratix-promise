974c757dd6d4c02c7d958dafa527d5dd8de19f7d
Howard <139238193+howard-oc@users.noreply.github.com>
2024-05-03 11:46:45 +0100

    refactor: changed logging in main and format of promise to add jarURI

--- internal/configure-pipeline/tests/mod.rs
+++ internal/configure-pipeline/tests/mod.rs
@@ -1,9 +1,6 @@
-use kratix_utils::pipeline::load_file;
 use kratix_utils::pipeline::PipelineConfig;
 use kratix_utils::ResourceRequest;
-use serde_yaml::{to_string, Value};
-use std::fs::read_to_string;
-use std::path::Path;
+use std::{env, fs, path::Path};
 
 #[derive(Clone)]
 pub struct MyPromise {
@@ -22,8 +19,34 @@ impl ResourceRequest for MyPromise {
 mod tests {
     use super::*;
 
+    const KRATIX_WORKFLOW_TYPE: &str = "KRATIX_WORKFLOW_TYPE";
+
+    #[test]
+    fn test_promise_request() -> Result<(), String> {
+        init_logger();
+
+        env::set_var(KRATIX_WORKFLOW_TYPE, "promise");
+
+        let request = MyPromise {
+            params: String::from("(custom)"),
+        };
+
+        let _result = kratix_utils::run_custom_pipeline(Some(request));
+
+        let new_kout_path = format!("{}/cluster-operator.yml", _result.kratix_output_dir());
+        assert_eq!(Path::new(&new_kout_path).exists(), true);
+
+        let _ = teardown(_result.kratix_output_dir());
+
+        Ok(())
+    }
+
     #[test]
     fn test_resource_request() -> Result<(), String> {
+        init_logger();
+
+        env::set_var(KRATIX_WORKFLOW_TYPE, "resource");
+
         let request = MyPromise {
             params: String::from("(custom)"),
         };
@@ -32,6 +55,25 @@ mod tests {
 
         let new_kout_path = format!("{}/flink-instance.yaml", _result.kratix_output_dir());
         assert_eq!(Path::new(&new_kout_path).exists(), true);
+
+        let _ = teardown(_result.kratix_output_dir());
+
         Ok(())
+    }
+
+    fn init_logger() {
+        let _ = env_logger::builder()
+            // Include all events in tests
+            .filter_level(log::LevelFilter::max())
+            // Ensure events are captured by `cargo test`
+            .is_test(true)
+            // Ignore errors initializing the logger if tests race to configure it
+            .try_init();
+    }
+
+    fn teardown(path: &str) {
+        fs::remove_dir_all(path).unwrap();
+        fs::create_dir(path).unwrap();
+        env::remove_var(KRATIX_WORKFLOW_TYPE);
     }
 }
